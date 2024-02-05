f933cab2ee55e87a4018d7859571dd414b97098a
0xAungkon <only.coder000@gmail.com>
2024-02-06 03:48:55 +0600

    just notes

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en" class=" sizes customelements history pointerevents postmessage webgl websockets cssanimations csscolumns csscolumns-width csscolumns-span csscolumns-fill csscolumns-gap csscolumns-rule csscolumns-rulecolor csscolumns-rulestyle csscolumns-rulewidth csscolumns-breakbefore csscolumns-breakafter csscolumns-breakinside flexbox picture srcset webworkers"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Aungkon Malakar | Onlycoder | Programmer | Developer | 0xAungkon</title>
+    <meta name="robots" content="noindex, follow">
+    <meta name="title" content="Aungkon Malakar | Onlycoder | Programmer | Developer | 0xAungkon">
+    <meta name="description" content="Discover the coding journey of 0xAungkon Malakar Joy, a self-taught programmer specializing in PHP and Python, through his personal portfolio website. ">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link rel="shortcut icon" type="image/x-icon" href="./assets/favicon.ico">
+    <style>
+        .rn-about-area {
+            cursor: pointer;
+        }
+        .rn-about-area h6{
+            transition-duration: 1s;
+        }
+        .rn-about-area:hover h6{
+            color:#ff014f !important;
+        }
+        .social_icons img{
+            width: 30px;
+            margin-right: 20px;
+        }
+    </style>
+    <!-- CSS 
+    ============================================ -->
+    <link rel="stylesheet" href="./assets/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/slick.css">
+    <link rel="stylesheet" href="./assets/slick-theme.css">
+    <link rel="stylesheet" href="./assets/aos.css">
+    <link rel="stylesheet" href="./assets/feature.css">
+    
+    <link rel="stylesheet" href="./assets/style.css">
+<style type="text/css" id="tts-styles">[data-tts-block-id].tts-active {background: rgba(206, 225, 255, 0.9) !important;} [data-tts-sentence-id].tts-active {background: rgba(0, 89, 191, 0.7) !important;}</style>
+<style>
+    
+    .banner-details-wrapper-sticky .thumbnail img{
+        width:240px;
+        height:240px;
+    }
+    .sticky-home-wrapper .rn-skill-area .inner .skill-share-inner .skill-share li{
+        position:relative;
+    }
+    .sticky-home-wrapper .rn-skill-area .inner .skill-share-inner .skill-share li.t{
+        width: 100%;
+        height: 40px;
+        box-shadow: none;
+        text-align: left;
+        justify-content: left;
+        padding-left: 10px;
+        margin-bottom: 0px;
+        background: transparent;
+        font-weight: bolder;
+        
+    }
+    
+.tooltiptext {
+    width: 291px;
+    color: #fff;
+    text-align: center;
+    position: absolute;
+    padding: 10px !important;
+    z-index: 1;
+    bottom: 116%;
+    left: -53%;
+    margin-left: -60px;
+    line-height: normal;
+    border-radius: 6px;
+    font-size: 0.7em;
+    background: var(--background-color-1);
+    box-shadow: var(--shadow-1);
+}
+
+.tooltiptext::after {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 50%;
+    margin-left: -11px;
+    border-width: 11px;
+    border-style: solid;
+    border-color: #23272b transparent transparent transparent;
+}
+
+.tooltip .tooltiptext {
+  visibility: visible;
+}
+.mw-page-title-main{
+    width: 100%;
+    display: block;
+    font-size: 1.6em;
+    margin-bottom: 6px;
+    margin-top: 4px;
+}
+.skill2 .tooltiptext::after{
+    margin-left: -30px;
+}
+#dd{
+    font-size: 1.4em;
+    color:white;
+    margin-top: -10px;
+    opacity: 0.8;
+    font-weight: 600;
+}
+</style>
+
+</head>
+
+
+<body class="template-color-1 home-sticky spybody" data-spy="scroll" data-target=".navbar-example2" data-offset="150" data-aos-easing="ease" data-aos-duration="400" data-aos-delay="0">
+    <main class="main-page-wrapper">
+        
+       
+         <div class="rn-slider-area">
+            <div class="container">
+                <div class="row row--30 pt--100 pt_sm--50">
+                    <div class="col-lg-6">
+                        <div class="d-flex flex-wrap align-content-start h-100">
+
+                                <div class="sticky-top-slider position-sticky sticky-top rbt-sticky-top-adjust-two" style="top: 150px;">
+                                <div class="banner-details-wrapper-sticky slide">
+                                    <div class="thumbnail">
+                                        <img src="./assets/banner-06.png" alt="Personal Portfolio">
+                                    </div>
+                                    <div class="banner-title-area pt--30">
+                                        <h1 class="title" style="font-family: Montserrat, Bangla161, sans-serif;">Hi, I’m <br><span>Aungkon Malakar</span><br></h1>
+                                        <p  id='dd'> Python Dev , Wordpress Bull & CI/CD  Enthusiast.</p>
+                                        <p class="disc">A Programmer passionate about AI, Cybersecurity, and problem-solving. With a background in business and experience With More Then a Half Decade, I specialize in Web Application Development.</p>
+                                    </div>
+                                    
+                                    <div class="d-flex p-3 mt-4 social_icons">
+                                        <div class="p-2"><a href="https://github.com/onlycoder000" target="_blank"><img src="./assets/gh.png" alt="Icons Images"></a></div>
+                                        <div class="p-2"><a href="https://tryhackme.com/p/0xAungkon" target="_blank"><img src="./assets/tryhackme_logo_full.svg" alt="Icons Images"></a></div>
+                                        <div class="p-2"><a href="mailto:aungkonmalakar@gmail.com" target="_blank"><img src="./assets/mail.png" alt="Icons Images"></a></div>
+                                        <div class="p-2"><a href="https://api.whatsapp.com/send?phone=8801303518838" target="_blank"><img src="./assets/wh.png" alt="Icons Images"></a></div>
+                                      </div>
+                                      <div class="button-group text-center text-md-start pt--40 pt_md--40 pt_sm--40">
+                                        <a class="rn-btn" href="https://api.whatsapp.com/send?phone=8801303518838" target="_blank"><span>Say Hello !!</span></a>
+                                        <a class="rn-btn" href="mailto:aungkonmalakar@gmail.com" target="_blank"><span>Need Consulatation ?</span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="sticky-home-wrapper">
+
+                            
+                            <div class="rn-about-area section-height">
+                                <div class="inner">
+                                    <h5 class="title" style="font-family: Montserrat, Bangla161, sans-serif;">
+                                        About Me
+                                    </h5>
+                                    <p class="about-disc">
+                                        Hi I am a Full-Stack Web developer &amp; Automation Developer, I can build your Web Application or Automation to convert your tasks to automation flow that AI will do your job for you.
+                                        As well , I can develope plugin &amp; wordpress theme. And I have three years of experience in
+                                        Git and Linux server Management.
+                                    </p>
+                                    <p class="about-disc">
+                                        <h6 style="text-transform: uppercase;color: var(--color-body);line-height: 35px;font-size: 1.2em;">“ Trust Is My Nature <br> Passion is my weapon <br> Hardwork is my shild ”</h6>
+                                    </p>
+                                </div>
+                            </div>
+                            
+
+                            
+                            <div class="rn-skill-area section-height">
+                                <div class="inner slide">
+                                    <h5 class="title" style="font-family: Montserrat, Bangla161, sans-serif;">
+                                        About My Skill
+                                    </h5>
+                                    <div class="skill-share-inner pt--100">
+                                        <span class="title">Primary Skills on</span>
+                                        <ul class="skill-share d-flex liststyle">
+                                            <li class='t'>Frontend</li>
+                                            <li data-src='HTML'><img src="https://skillicons.dev/icons?i=html" alt="HTML"></li>
+                                            <li data-src='CSS'><img src="https://skillicons.dev/icons?i=css" alt="CSS"></li>
+                                            <li data-src='JavaScript'><img src="https://skillicons.dev/icons?i=javascript" alt="JAVASCRIPT"></li>
+                                            <li data-src='React_(software)'><img src="https://skillicons.dev/icons?i=react" alt="Icons Images"></li>
+                                            <li class='t'>Python Backend</li>
+                                            <li data-src='Python_(programming_language)'><img src="https://skillicons.dev/icons?i=python" alt="Icons Images"></li>
+                                            <li data-src='Django_(web_framework)'><img src="https://skillicons.dev/icons?i=django" alt="Icons Images"></li>
+                                            <li data-src='Flask_(web_framework)'><img src="https://skillicons.dev/icons?i=flask" alt="Icons Images"></li>
+                                            <li data-src='FastAPI'><img src="https://fastapi.tiangolo.com/img/icon-white.svg" alt="Icons Images"></li>
+                                            <li class='t'>PHP Backend</li>
+                                            <li data-src='PHP'><img src="https://skillicons.dev/icons?i=php" alt="Icons Images"></li>
+                                            <li data-src='WordPress'><img src="https://skillicons.dev/icons?i=wordpress" alt="Icons Images"></li>
+                                            <li data-src='MySQL'><img src="https://skillicons.dev/icons?i=mysql" alt="Icons Images"></li>
+                                            <li data-src='PostgreSQL'><img src="https://skillicons.dev/icons?i=postgresql" alt="Icons Images"></li>
+                                        </ul>
+                                    </div>
+                                    <div class="skill-share-inner skill-share-inner-width margin-top-25 skill2">
+                                        <span class="title">Secondary Skills on</span>
+                                        <ul class="skill-share d-flex liststyle">
+                                            <li data-src='Amazon_Web_Services'><img src="https://skillicons.dev/icons?i=aws" alt="Icons Images"></li>
+                                            <li data-src='Docker_(software)'><img src="https://skillicons.dev/icons?i=docker" alt="Icons Images"></li>
+                                            <li data-src='Google_Cloud_Platform'><img src="https://skillicons.dev/icons?i=gcp" alt="Icons Images"></li>
+                                            <li data-src='Git'><img src="https://skillicons.dev/icons?i=git" alt="Icons Images"></li>
+                                            <li data-src='GitLab'><img src="https://skillicons.dev/icons?i=gitlab" alt="Icons Images"></li>
+                                            <li data-src='Visual_Studio_Code'><img src="https://skillicons.dev/icons?i=vscode" alt="Icons Images"></li>
+                                            <li data-src='Linux'><img src="https://skillicons.dev/icons?i=linux" alt="Icons Images"></li>
+                                            <li data-src='Postman_(software)'><img src="https://skillicons.dev/icons?i=postman" alt="Icons Images"></li>
+                                            <li data-src='Bash_(Unix_shell)'><img src="https://skillicons.dev/icons?i=bash" alt="Icons Images"></li>
+                                            <li data-src='Kubernetes'><img src="https://skillicons.dev/icons?i=k8s" alt="Icons Images"></li>
+                                            <li data-src='Microsoft_Azure'><img src="https://skillicons.dev/icons?i=azure" alt="Icons Images"></li>
+                                            <li data-src='PyTorch'><img src="https://skillicons.dev/icons?i=pytorch" alt="Icons Images"></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            
+
+                            
+                            
+
+                            
+                            
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+        <div class="backto-top" style="opacity: 1;">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up" style="font-family: feather, Bangla161, sans-serif;"><line x1="12" y1="19" x2="12" y2="5" style="font-family: feather, Bangla161, sans-serif;"></line><polyline points="5 12 12 5 19 12" style="font-family: feather, Bangla161, sans-serif;"></polyline></svg>
+            </div>
+        </div>
+        
+        
+        
+        
+
+        
+        
+        
+    </main>
+    <script>
+        document.querySelectorAll('.rn-skill-area .skill-share-inner  ul li[data-src]').forEach((nod)=>{
+            nod.addEventListener("mouseenter", function() {
+                if(!nod.querySelector('span')){
+                    var span=document.createElement('span');
+                    
+                    span.setAttribute('class','tooltiptext');
+                    span.innerHTML='<img src="loader.gif">';
+                    nod.appendChild(span);
+                    // HTML data to send in the POST request
+                    var htmlData = nod.getAttribute('data-src');
+                    if(htmlData){
+                        const url = './files/'+htmlData+'.json';
+                        var xhr = new XMLHttpRequest();
+                        xhr.open('GET', url, true);
+                        xhr.responseType = 'json';
+                        
+                        xhr.onload = function() {
+                            var data=xhr.response;
+                            // data=JSON.parse(data);
+                            try {
+                                var r=data.displaytitle+'<p1>'+data.extract+'</p1>';
+                                span.innerHTML=r;
+                            } catch (error) {
+                                // Code to handle the exception
+                                span.innerHTML='<span class="mw-page-title-main">'+htmlData+'</span>';
+                            }
+                            
+                        };
+                        
+                        xhr.send();
+                    };
+                    
+                    // Make the POST request using the fetch API
+                    
+                    
+                        
+                    // Create the request options
+                }
+                
+                
+                  
+            });
+            nod.addEventListener("mouseleave", function() {
+                nod.querySelector('span').remove();
+            });
+            
+        })
+    </script>
+    <script src="./assets/jquery.js"></script>
+    <script src="./assets/modernizer.min.js"></script>
+    <script src="./assets/feather.min.js"></script>
+    <script src="./assets/slick.min.js"></script>
+    <script src="./assets/bootstrap.js"></script>
+    <script src="./assets/text-type.js"></script>
+    <script src="./assets/wow.js"></script>
+    <script src="./assets/aos.js"></script>
+    <script src="./assets/particles.js"></script>
+    <script src="./assets/jquery-one-page-nav.js"></script>
+    
+    <script src="./assets/main.js"></script>
+
+
+<style>		@font-face {				font-family: Bangla161;				src: local('Siyam Rupali');				unicode-range: U+0980-09FF;		}		body {			font-family: Bangla161, Poppins, sans-serif;		}	</style></body></html>
